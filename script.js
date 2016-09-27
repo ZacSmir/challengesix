0c3867575e8b389c9ff5cc4b784aa25fe25944c9
ZacSmir <Zach@smirin.us>
2016-09-27 08:39:07 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var quantityInput = $(".element-quantity-input")
 
 quantityInput.keydown(function(e) {
-  var keyCode = 1;
-  console.log
+  var keyCode = 13;
+  console.log(keyCode)
   var inputValue = quantityInput.val();
 
   // enter key!
@@ -12,7 +12,7 @@ quantityInput.keydown(function(e) {
 })
 
 function addElementsToThePage(numberOfElements) {	  
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < 1; i++) {
     var newEl = $("<div class='new-el'>Hi there!</div>");		
     $(".element-display").append(newEl);
   }
